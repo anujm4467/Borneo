2f18331a61356bb09eb4459182f038387d515ecb
Anuj Mishra <anujm4467@gmail.com>
2022-04-09 10:24:37 +0530

    first commit

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,13 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // append the prefix api in all route controller
+
   app.setGlobalPrefix('api');
 
   app.enableCors();
+
+  //helps you secure your Express apps by setting various HTTP headers.
+
+  app.use(helmet());
+
+  // avoid the multiple request in a short period of time
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  });
+
+  // Apply the rate limiting middleware to all requests
+
+  app.use(limiter);
 
   const options = new DocumentBuilder()
     .setTitle('NestJS API Gateway')
